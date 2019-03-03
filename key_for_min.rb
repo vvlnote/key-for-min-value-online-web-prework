ce9941c32d2e0ea471b951e79f3868bdc0a563a8
Joy Lin <vvlnote@hotmail.com>
2019-03-03 17:42:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,15 +5,15 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
     nil
   else
-    smallest_num = nil
-    smallest_num_key = nil
-    name_hash.collect do |key, value|
-      if smallest_num == nil
-        smallest_num = value
-        smallest_num_key = key
-      elsif smallest_num > value
-        smallest_num = value
-        smallest_num_key = key
-      end
+#    smallest_num = nil
+#    smallest_num_key = nil
+#    name_hash.collect do |key, value|
+#      if smallest_num == nil
+#        smallest_num = value
+#        smallest_num_key = key
+#      elsif smallest_num > value
+#        smallest_num = value
+#        smallest_num_key = key
+#      end
   end
 end
